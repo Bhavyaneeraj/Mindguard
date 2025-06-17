64a122d9780eb6e4c309bc450e6fb6c848e2fb1c
Ravuri Bhavya Neeraj <bannubhavyaneeraj@gmail.com>
2025-06-17 18:21:44 +0530

    Set default values for EMAIL_HOST_USER and EMAIL_HOST_PASSWORD

--- browser_tracker/settings.py
+++ browser_tracker/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='unsafe-key')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
