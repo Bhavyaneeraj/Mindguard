2b5d1f65a004a95e9aee78d870dc2cc182b237d7
Ravuri Bhavya Neeraj <bannubhavyaneeraj@gmail.com>
2025-06-17 14:50:50 +0530

    Remove vercel.json to avoid deployment conflicts

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Point to your Django project
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "browser_tracker.settings")
-
-from django.core.wsgi import get_wsgi_application
-app = get_wsgi_application()
