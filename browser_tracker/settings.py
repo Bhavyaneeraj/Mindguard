5e8441766f3a1590ef303e4cf02a5f35d0f742d2
Ravuri Bhavya Neeraj <bannubhavyaneeraj@gmail.com>
2025-06-17 18:26:04 +0530

    Set default values for EMAIL_HOST_USER and EMAIL_HOST_PASSWORD

--- browser_tracker/settings.py
+++ browser_tracker/settings.py
@@ -135,8 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
